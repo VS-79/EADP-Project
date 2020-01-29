cc22d8652fdca2013329bba00b56d5c9d988b982
Fulmilation <45026591+Fulmilation@users.noreply.github.com>
2020-01-29 13:44:41 +0800

    Add files via upload

---
+++ BLL/Foods.cs
@@ -0,0 +1,84 @@
+﻿using EADP_Project.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EADP_Project.BLL
+{
+    public class Foods
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Culinarytype { get; set; }
+        public string Deals { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Opwkd1 { get; set; }
+        public string Opwkd2 { get; set; }
+        public string Opwke1 { get; set; }
+        public string Opwke2 { get; set; }
+        public string Pricewkd1 { get; set; }
+        public string Pricewkd2 { get; set; }
+        public string Pricewke1 { get; set; }
+        public string Pricewke2 { get; set; }
+        public string Lat1 { get; set; }
+        public string Long1 { get; set; }
+        public string Image { get; set; }
+        public string Userid { get; set; }
+        public Foods()
+        {
+
+        }
+        public Foods(string id, string name, string description, string culinarytype,string deals,string email, string phone, string opwkd1, string opwkd2, string opwke1, string opwke2, string pricewkd1, string pricewkd2, string pricewke1, string pricewke2, string lat1, string long1, string image, string userid)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Culinarytype = culinarytype;
+            this.Deals = deals;
+            this.Email = email;
+            this.Phone = phone;
+            this.Opwkd1 = opwkd1;
+            this.Opwkd2 = opwkd2;
+            this.Opwke1 = opwke1;
+            this.Opwke2 = opwke2;
+            this.Pricewkd1 = pricewkd1;
+            this.Pricewkd2 = pricewkd2;
+            this.Pricewke1 = pricewke1;
+            this.Pricewke2 = pricewke2;
+            this.Lat1 = lat1;
+            this.Long1 = long1;
+            this.Image = image;
+            this.Userid = userid;
+        }
+        public Foods GetFoodById(string id)
+        {
+            FoodsDAO dao = new FoodsDAO();
+            return dao.SelectById(id);
+        }
+
+        public List<Foods> GetFoodAll()
+        {
+            FoodsDAO dao = new FoodsDAO();
+            return dao.SelectAll();
+        }
+
+        public int SetFood(string name, string description, string culinarytype, string deals, string email, string phone, string opwkd1, string opwkd2, string opwke1, string opwke2, string pricewkd1, string pricewkd2, string pricewke1, string pricewke2, string lat1, string long1, string image, string userid)
+        {
+            FoodsDAO dao = new FoodsDAO();
+            return dao.InsertFood(name, description, culinarytype, deals, email, phone, opwkd1, opwkd2, opwke1, opwke2, pricewkd1, pricewkd2, pricewke1, pricewke2, lat1, long1, image, userid);
+        }
+        public int UpdateFood(string id,string name, string description, string culinarytype, string deals, string email, string phone, string opwkd1, string opwkd2, string opwke1, string opwke2, string pricewkd1, string pricewkd2, string pricewke1, string pricewke2, string lat1, string long1, string image, string userid)
+        {
+            FoodsDAO dao = new FoodsDAO();
+            return dao.FoodUpdate(id,name, description, culinarytype, deals, email, phone, opwkd1, opwkd2, opwke1, opwke2, pricewkd1, pricewkd2, pricewke1, pricewke2, lat1, long1, image, userid);
+        }
+        public int DeleteFood(string id)
+        {
+            FoodsDAO dao = new FoodsDAO();
+            return dao.Fooddelete(id);
+        }
+    }
+}
---
+++ DAL/FoodsDAO.cs
@@ -0,0 +1,202 @@
+﻿using EADP_Project.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EADP_Project.DAL
+{
+    public class FoodsDAO
+    {
+        public Foods SelectById(string foodId)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            string sqlstmt = "Select * from Food where id = @paraId";
+            SqlDataAdapter da = new SqlDataAdapter(sqlstmt, myConn);
+            da.SelectCommand.Parameters.AddWithValue("@paraId", foodId);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            int rec_cnt = ds.Tables[0].Rows.Count;
+
+            Foods obj = null;
+            if (rec_cnt > 0)
+            {
+                DataRow row = ds.Tables[0].Rows[0];
+                string id = row["id"].ToString();
+                string name = row["name"].ToString(); ;
+                string description = row["description"].ToString();
+                string culinarytype = row["culinarytype"].ToString();
+                string deals = row["deals"].ToString();
+                string email = row["email"].ToString();
+                string phone = row["phone"].ToString();
+                string opwkd1 = row["opwkd1"].ToString();
+                string opwkd2 = row["opwkd2"].ToString();
+                string opwke1 = row["opwke1"].ToString();
+                string opwke2 = row["opwke2"].ToString();
+                string pricewkd1 = row["pricewkd1"].ToString();
+                string pricewkd2 = row["pricewkd2"].ToString();
+                string pricewke1 = row["pricewke1"].ToString(); ;
+                string pricewke2 = row["pricewke2"].ToString();
+                string lat1 = row["lat1"].ToString();
+                string long1 = row["long1"].ToString();
+                string image = row["image"].ToString();
+                string userid = row["userid"].ToString();
+                obj = new Foods(id, name, description, culinarytype,deals, email, phone, opwkd1, opwkd2, opwke1, opwke2, pricewkd1, pricewkd2, pricewke1, pricewke2, lat1, long1, image, userid);
+            }
+            return obj;
+        }
+
+        public int InsertFood(string name, string description,string culinarytype, string deals, string email, string phone, string opwkd1, string opwkd2, string opwke1, string opwke2, string pricewkd1, string pricewkd2, string pricewke1, string pricewke2, string lat1, string long1, string image, string userid)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            string sqlstmt = "Insert into Food(name, description,culinarytype,deals,email, phone, opwkd1, opwkd2, opwke1, opwke2, pricewkd1, pricewkd2, pricewke1, pricewke2, lat1, long1, image, userid) Values(@paraname, @paradescription,@paraculinarytype,@paradeals,@paraemail, @paraphone, @paraopwkd1, @paraopwkd2, @paraopwke1, @paraopwke2, @parapricewkd1, @parapricewkd2, @parapricewke1, @parapricewke2, @paralat1, @paralong1, @paraimage, @parauserid)";
+            SqlCommand sqlCmd = new SqlCommand(sqlstmt, myConn);
+
+            sqlCmd.Parameters.AddWithValue("@paraname", name);
+            sqlCmd.Parameters.AddWithValue("@paradescription", description);
+            sqlCmd.Parameters.AddWithValue("@paraculinarytype", culinarytype);
+            sqlCmd.Parameters.AddWithValue("@paradeals", deals);
+            sqlCmd.Parameters.AddWithValue("@paraemail", email);
+            sqlCmd.Parameters.AddWithValue("@paraphone", phone);
+            sqlCmd.Parameters.AddWithValue("@paraopwkd1", opwkd1);
+            sqlCmd.Parameters.AddWithValue("@paraopwkd2", opwkd2);
+            sqlCmd.Parameters.AddWithValue("@paraopwke1", opwke1);
+            sqlCmd.Parameters.AddWithValue("@paraopwke2", opwke2);
+            sqlCmd.Parameters.AddWithValue("@parapricewkd1", pricewkd1);
+            sqlCmd.Parameters.AddWithValue("@parapricewkd2", pricewkd2);
+            sqlCmd.Parameters.AddWithValue("@parapricewke1", pricewke1);
+            sqlCmd.Parameters.AddWithValue("@parapricewke2", pricewke2);
+            sqlCmd.Parameters.AddWithValue("@paralat1", lat1);
+            sqlCmd.Parameters.AddWithValue("@paralong1", long1);
+            sqlCmd.Parameters.AddWithValue("@paraimage", image);
+            sqlCmd.Parameters.AddWithValue("@parauserid", userid);
+
+            myConn.Open();
+            int result = sqlCmd.ExecuteNonQuery();
+
+            myConn.Close();
+
+            return result;
+
+
+        }
+
+        public List<Foods> SelectAll()
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            String sqlstmt = "SELECT * From Food where valid = 1";
+
+            SqlDataAdapter da = new SqlDataAdapter(sqlstmt, myConn);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            List<Foods> fdList = new List<Foods>();
+
+            int rec_cnt = ds.Tables[0].Rows.Count;
+            if (rec_cnt == 0)
+            {
+                fdList = null;
+            }
+            else
+            {
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    string id = row["id"].ToString();
+                    string name = row["name"].ToString(); ;
+                    string description = row["description"].ToString();
+                    string culinarytype = row["culinarytype"].ToString();
+                    string deals = row["deals"].ToString();
+                    string email = row["email"].ToString();
+                    string phone = row["phone"].ToString();
+                    string opwkd1 = row["opwkd1"].ToString();
+                    string opwkd2 = row["opwkd2"].ToString();
+                    string opwke1 = row["opwke1"].ToString();
+                    string opwke2 = row["opwke2"].ToString();
+                    string pricewkd1 = row["pricewkd1"].ToString();
+                    string pricewkd2 = row["pricewkd2"].ToString();
+                    string pricewke1 = row["pricewke1"].ToString(); ;
+                    string pricewke2 = row["pricewke2"].ToString();
+                    string lat1 = row["lat1"].ToString();
+                    string long1 = row["long1"].ToString();
+                    string image = row["image"].ToString();
+                    string userid = row["userid"].ToString();
+                    Foods objFood = new Foods(id, name, description, culinarytype,deals, email, phone, opwkd1, opwkd2, opwke1, opwke2, pricewkd1, pricewkd2, pricewke1, pricewke2, lat1, long1, image, userid);
+                    fdList.Add(objFood);
+                }
+            }
+            return fdList;
+        }
+        public int FoodUpdate(string id, string name, string description, string culinarytype, string deals, string email, string phone, string opwkd1, string opwkd2, string opwke1, string opwke2, string pricewkd1, string pricewkd2, string pricewke1, string pricewke2, string lat1, string long1, string image, string userid)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            string sqlStmt = "UPDATE Food SET name = @paraname, description = @paradescription,culinarytype = @paraculinarytype,deals = @paradeals,email = @paraemail, phone = @paraphone, opwkd1 = @paraopwkd1, opwkd2 = @paraopwkd2, " +
+                "opwke1 = @paraopwke1, opwke2 = @paraopwke2, pricewkd1 = @parapricewkd1, pricewkd2 = @parapricewkd2, pricewke1 = @parapricewke1, pricewke2 = @parapricewke2, lat1 = @paralat1, long1 = @paralong1, image = @paraimage, userid = @parauserid where id =  @paraid";
+
+            int result = 0;    // Execute NonQuery return an integer value
+            SqlCommand sqlCmd = new SqlCommand(sqlStmt, myConn);
+
+
+            sqlCmd = new SqlCommand(sqlStmt.ToString(), myConn);
+            sqlCmd.Parameters.AddWithValue("@paraid", id);
+            sqlCmd.Parameters.AddWithValue("@paraname", name);
+            sqlCmd.Parameters.AddWithValue("@paradescription", description);
+            sqlCmd.Parameters.AddWithValue("@paraculinarytype", culinarytype);
+            sqlCmd.Parameters.AddWithValue("@paradeals", deals);
+            sqlCmd.Parameters.AddWithValue("@paraemail", email);
+            sqlCmd.Parameters.AddWithValue("@paraphone", phone);
+            sqlCmd.Parameters.AddWithValue("@paraopwkd1", opwkd1);
+            sqlCmd.Parameters.AddWithValue("@paraopwkd2", opwkd2);
+            sqlCmd.Parameters.AddWithValue("@paraopwke1", opwke1);
+            sqlCmd.Parameters.AddWithValue("@paraopwke2", opwke2);
+            sqlCmd.Parameters.AddWithValue("@parapricewkd1", pricewkd1);
+            sqlCmd.Parameters.AddWithValue("@parapricewkd2", pricewkd2);
+            sqlCmd.Parameters.AddWithValue("@parapricewke1", pricewke1);
+            sqlCmd.Parameters.AddWithValue("@parapricewke2", pricewke2);
+            sqlCmd.Parameters.AddWithValue("@paralat1", lat1);
+            sqlCmd.Parameters.AddWithValue("@paralong1", long1);
+            sqlCmd.Parameters.AddWithValue("@paraimage", image);
+            sqlCmd.Parameters.AddWithValue("@parauserid", userid);
+
+            myConn.Open();
+            result = sqlCmd.ExecuteNonQuery();
+
+            myConn.Close();
+
+            return result;
+        }
+        public int Fooddelete(string id)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            string sqlStmt = "UPDATE Food SET valid = 0 where id =  @paraid";
+
+            int result = 0;    // Execute NonQuery return an integer value
+            SqlCommand sqlCmd = new SqlCommand(sqlStmt, myConn);
+
+
+            sqlCmd = new SqlCommand(sqlStmt.ToString(), myConn);
+
+            sqlCmd.Parameters.AddWithValue("@paraid", id);
+            myConn.Open();
+            result = sqlCmd.ExecuteNonQuery();
+
+            myConn.Close();
+
+            return result;
+        }
+    }
+}
